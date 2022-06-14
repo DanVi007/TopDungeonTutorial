6ff9c6603b9ee7883384b080112671857382ecaa
DanVi007 <danvitrinh@outlook.com>
2022-06-14 20:59:25 +0200

    finished:section 5

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -9,6 +9,7 @@ public class Portal : Collidable
     {
         if (coll.name == "Player")
         {
+            GameManager.instance.SaveState();
             // Teleport the player
             string sceneName = sceneNames[Random.Range(0,sceneNames.Length)];
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
