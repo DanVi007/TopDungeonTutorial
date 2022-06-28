4dc2d271013ad7bbb381c840e8499b0d51efabf1
DanVi007 <danvitrinh@outlook.com>
2022-06-28 13:49:52 +0200

    finished: skeleton for ui

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -12,7 +12,9 @@ public class Chest : Collectable
         {
             collected = true;
             GetComponent<SpriteRenderer>().sprite = emptyChest;
-            // +5 pesos!
+
+            GameManager.instance.pesos += pesosAmount;
+            
             GameManager.instance.ShowText("+" + pesosAmount + " pesos!", 25, 
              Color.yellow, transform.position, Vector3.up * 25, 1.5f);
 
